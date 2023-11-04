a4fad3909a73b32e33fd628b7c742ef462e56548
Lorenzo Pagani <larrypg2@gmail.com>
2023-11-04 12:37:35 +0100

    feat: adds clickable labels

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
-const shoppingList = [ "Mela",
-"Banana",
-"Arancia",
-"Fragola",
-"Ciliegia",
-"Uva",
-"Pesca",
-"Pera",
-"Kiwi",
-"Ananas",
-];
-const cart = document.getElementById("cart");
+const shoppingList = [
+  'Mela',
+  'Banana',
+  'Arancia',
+  'Fragola',
+  'Ciliegia',
+  'Uva',
+  'Pesca',
+  'Pera',
+  'Kiwi',
+  'Ananas'
+]
+const cart = document.getElementById('cart')
 let i = 0
-const checkbox = `<input type="checkbox" class= "checkbox" >`;
 while (i < shoppingList.length) {
-    const listItem = `<li class="list-group-item">${checkbox} ${shoppingList[i]}</li>`
-    cart.innerHTML += listItem;
-    /* if (i%2 == 0) {
+  const checkbox = `<input type="checkbox" class= "checkbox" id= "${shoppingList[i]}" >`
+  const listItem = `<li class="list-group-item">${checkbox} <label for="${shoppingList[i]}">${shoppingList[i]}</label></li>`
+  cart.innerHTML += listItem
+  /* if (i%2 == 0) {
         listItem.style.color = "red";
     } */
-    console.log(shoppingList[i]);
-    i++
+  console.log(shoppingList[i])
+  i++
 }
 
-  const checkboxes = document.querySelectorAll('.checkbox');
-  checkboxes.forEach(function(checkbox)  {
-    checkbox.addEventListener('click', function () {
-      const listItem = this.parentNode; 
+const checkboxes = document.querySelectorAll('.checkbox')
+checkboxes.forEach(function (checkbox) {
+  checkbox.addEventListener('click', function () {
+    const label = document.querySelector(`label[for="${this.id}"]`)
 
-      if (this.checked) {
-        listItem.classList.add('barred'); 
-      } else {
-        listItem.classList.remove('barred'); 
-      }
-    });
-  });
-
+    if (this.checked) {
+      label.classList.add('barred')
+    } else {
+      label.classList.remove('barred')
+    }
+  })
+})
