0118e9b5a3fb7898a78327e2d2f516a63bf2fbd2
Lorenzo Pagani <larrypg2@gmail.com>
2023-11-04 12:21:55 +0100

    feat: adds checkboxes style: adds colored layout

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-const shoppingList = ["pane", "latte", "affettati", "parmigiano",];
+const shoppingList = [ "Mela",
+"Banana",
+"Arancia",
+"Fragola",
+"Ciliegia",
+"Uva",
+"Pesca",
+"Pera",
+"Kiwi",
+"Ananas",
+];
 const cart = document.getElementById("cart");
 let i = 0
+const checkbox = `<input type="checkbox" class= "checkbox" >`;
 while (i < shoppingList.length) {
-    cart.innerHTML += `<li> ${shoppingList[i]}</li>`
+    const listItem = `<li class="list-group-item">${checkbox} ${shoppingList[i]}</li>`
+    cart.innerHTML += listItem;
+    /* if (i%2 == 0) {
+        listItem.style.color = "red";
+    } */
     console.log(shoppingList[i]);
-
     i++
 }
+
+  const checkboxes = document.querySelectorAll('.checkbox');
+  checkboxes.forEach(function(checkbox)  {
+    checkbox.addEventListener('click', function () {
+      const listItem = this.parentNode; 
+
+      if (this.checked) {
+        listItem.classList.add('barred'); 
+      } else {
+        listItem.classList.remove('barred'); 
+      }
+    });
+  });
+
